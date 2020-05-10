728d2984a4215b1e4f46a132b9e75d3cce87c9c1
Pixelastic <tim@pixelastic.com>
2020-05-10 15:21:53 +0200

    test(sort): Add consistent order of keys

--- docs/src/_data/tailwind.js
+++ docs/src/_data/tailwind.js
@@ -1,13 +1,11 @@
 const config = require('../../../lib/main.js');
 const _ = require('golgoth/lib/lodash');
+const sortKeys = require('../../../lib/helpers/sortKeys.js');
 
 module.exports = {
   getThemeKeys(item) {
-    return _.chain(config)
-      .get(`theme.${item}`)
-      .keys()
-      .sort()
-      .value();
+    const keys = _.keys(_.get(config, `theme.${item}`));
+    return sortKeys(keys);
   },
   getClasses(themeKey, prefix) {
     const keys = this.getThemeKeys(themeKey);
