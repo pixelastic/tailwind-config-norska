48fc22d1e9974b0db8e1722603f15387a9d13bf3
Pixelastic <tim@pixelastic.com>
2020-05-11 15:53:42 +0200

    docs(test): Add tests for line-height

--- screenshots/html_report/config.js
+++ screenshots/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Background_Colors_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Background_Colors_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Background_Colors_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Background_Colors_0_screenshot_0_desktop.png",
         "label": "Background Colors",
@@ -26,7 +26,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
         "label": "Border Colors",
@@ -48,7 +48,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
         "label": "Border Radius",
@@ -57,22 +57,20 @@ report({
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -64
+            "height": 0
           },
-          "misMatchPercentage": "5.94",
-          "analysisTime": 44
-        },
-        "diffImage": "../bitmaps_test/20200511-151553/failed_diff_tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
         "label": "Border Width",
@@ -81,22 +79,20 @@ report({
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -3349
+            "height": 0
           },
-          "misMatchPercentage": "35.95",
-          "analysisTime": 360
-        },
-        "diffImage": "../bitmaps_test/20200511-151553/failed_diff_tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Dimensions_Height_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Dimensions_Height_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Dimensions_Height_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Dimensions_Height_0_screenshot_0_desktop.png",
         "label": "Dimensions Height",
@@ -104,14 +100,21 @@ report({
         "url": "http://127.0.0.1:8083/dimensions-height/",
         "expect": 0,
         "viewportLabel": "desktop",
-        "error": "Reference file not found /home/tim/local/www/projects/tailwind-config-norska/screenshots/bitmaps_reference/tailwind-config-norska_Dimensions_Height_0_screenshot_0_desktop.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Dimensions_Width_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Dimensions_Width_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Dimensions_Width_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Dimensions_Width_0_screenshot_0_desktop.png",
         "label": "Dimensions Width",
@@ -119,14 +122,21 @@ report({
         "url": "http://127.0.0.1:8083/dimensions-width/",
         "expect": 0,
         "viewportLabel": "desktop",
-        "error": "Reference file not found /home/tim/local/www/projects/tailwind-config-norska/screenshots/bitmaps_reference/tailwind-config-norska_Dimensions_Width_0_screenshot_0_desktop.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200511-151553/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
         "label": "Font Sizes",
@@ -144,6 +154,36 @@ report({
         }
       },
       "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/tailwind-config-norska_Line_Heights_Custom_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Line_Heights_Custom_0_screenshot_0_desktop.png",
+        "selector": ".screenshot",
+        "fileName": "tailwind-config-norska_Line_Heights_Custom_0_screenshot_0_desktop.png",
+        "label": "Line Heights Custom",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:8083/line-heights-custom/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /home/tim/local/www/projects/tailwind-config-norska/screenshots/bitmaps_reference/tailwind-config-norska_Line_Heights_Custom_0_screenshot_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/tailwind-config-norska_Line_Heights_Default_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-155258/tailwind-config-norska_Line_Heights_Default_0_screenshot_0_desktop.png",
+        "selector": ".screenshot",
+        "fileName": "tailwind-config-norska_Line_Heights_Default_0_screenshot_0_desktop.png",
+        "label": "Line Heights Default",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:8083/line-heights-default/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /home/tim/local/www/projects/tailwind-config-norska/screenshots/bitmaps_reference/tailwind-config-norska_Line_Heights_Default_0_screenshot_0_desktop.png"
+      },
+      "status": "fail"
     }
   ],
   "id": "tailwind-config-norska"
