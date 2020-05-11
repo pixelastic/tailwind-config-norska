cbb1dcff579bbc8bb8b58105464153e54e3c025b
Pixelastic <tim@pixelastic.com>
2020-05-11 11:43:22 +0200

    test(fontSize): Add font-size test page

--- screenshots/html_report/config.js
+++ screenshots/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Backgrounds_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200510-151907/tailwind-config-norska_Backgrounds_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-114243/tailwind-config-norska_Backgrounds_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Backgrounds_0_screenshot_0_desktop.png",
         "label": "Backgrounds",
@@ -26,7 +26,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200510-151907/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-114243/tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Colors_0_screenshot_0_desktop.png",
         "label": "Border Colors",
@@ -48,7 +48,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200510-151907/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-114243/tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png",
         "label": "Border Width",
@@ -62,17 +62,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "33.33",
-          "analysisTime": 412
-        },
-        "diffImage": "../bitmaps_test/20200510-151907/failed_diff_tailwind-config-norska_Border_Width_0_screenshot_0_desktop.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
-        "test": "../bitmaps_test/20200510-151907/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-114243/tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
         "selector": ".screenshot",
         "fileName": "tailwind-config-norska_Border_Radius_0_screenshot_0_desktop.png",
         "label": "Border Radius",
@@ -90,6 +88,21 @@ report({
         }
       },
       "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
+        "test": "../bitmaps_test/20200511-114243/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
+        "selector": ".screenshot",
+        "fileName": "tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png",
+        "label": "Font Sizes",
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:8083/font-sizes/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "error": "Reference file not found /home/tim/local/www/projects/tailwind-config-norska/screenshots/bitmaps_reference/tailwind-config-norska_Font_Sizes_0_screenshot_0_desktop.png"
+      },
+      "status": "fail"
     }
   ],
   "id": "tailwind-config-norska"
