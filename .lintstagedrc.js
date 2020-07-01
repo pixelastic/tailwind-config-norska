ac290d3f0bedb91af14e18d777961dec05f94586
Pixelastic <tim@pixelastic.com>
2020-07-01 23:06:11 +0200

    dev(lint): Auto lint files on commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,8 @@
-const lintStagedConfig = require('aberlaas/lib/configs/lintstaged.js');
 module.exports = {
-  ...lintStagedConfig,
+  '*.css': ['./scripts/meta/lint --css --fix'],
+  '*.yml': ['./scripts/meta/lint --yml --fix'],
+  '*.circleci/config.yml': ['./scripts/meta/lint --circleci'],
+  '*.json': ['./scripts/meta/lint --json --fix'],
+  '*.js': ['./scripts/meta/lint --js --fix'],
+  'modules/*/lib/**/*.js': './scripts/meta/test --failFast --findRelatedTests',
 };
--- modules/tailwind-config-norska-docs/norska.config.js
+++ modules/tailwind-config-norska-docs/norska.config.js
@@ -1,7 +1,26 @@
+const libPath = 'modules/tailwind-config-norska';
+const docsPath = 'modules/tailwind-config-norska-docs';
 module.exports = {
   port: 8087,
   cloudinary: {
     bucketName: null,
     enable: false,
+  },
+  netlify: {
+    deploy: {
+      files: [
+        'netlify.toml',
+        '.nvmrc',
+        'README.md',
+        `${docsPath}/norska.config.js`,
+        `${docsPath}/tailwind.config.js`,
+        `${docsPath}/src/**/*`,
+        `${docsPath}/lib/*.js`,
+        `${libPath}/lib/*.js`,
+        `${libPath}/lib/helpers/*.js`,
+        `${libPath}/lib/plugins/**/*.js`,
+        `${libPath}/lib/themes/**/*.js`,
+      ],
+    },
   },
 };
